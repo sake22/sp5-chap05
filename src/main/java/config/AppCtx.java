4a78bcd3ec232a1ac2f8fe12a6d8d2e29b9cd7e1
sake22 <sucm1234@gmail.com>
2021-07-11 17:58:36 +0900

    part 5,6

--- src/main/java/config/AppCtx.java
+++ src/main/java/config/AppCtx.java
@@ -3,10 +3,12 @@ package config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import spring.*;
 
 @Configuration
+@ComponentScan(basePackages = {"spring"})
 public class AppCtx {
 
     @Bean
